3594c863d7afe9b7d8bb808ab9f8b6f7d15a0dc8
Aidan Boland <aidanboland1@icloud.com>
2023-06-26 22:46:47 +0100

    made it look nice

--- src/next-track.tsx
+++ src/next-track.tsx
@@ -2,8 +2,9 @@ import { Detail } from "@raycast/api"
 import { execSync } from "child_process"
 
 export default function Command() {
-    let output = execSync("/opt/homebrew/bin/mpc next", { encoding: "utf8" });
+    let output: string = execSync("/opt/homebrew/bin/mpc next", { encoding: "utf8" });
+    output = output.substring(0, output.indexOf("[playing]"))
     return (
-        <Detail markdown={output} />
+        <Detail markdown={`**Now Playing:** ${output} \n Refresh (⌘R) to go to next`} />
     )
 }
