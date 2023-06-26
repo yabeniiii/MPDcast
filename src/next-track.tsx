f9b61e780277f9ee8b17af5b083e2c1f9ac7a071
Aidan Boland <aidanboland1@icloud.com>
2023-06-26 21:48:14 +0100

    next track command works, know how it's all gonna work

--- src/next-track.tsx
+++ src/next-track.tsx
@@ -1,1 +1,9 @@
+import { Detail } from "@raycast/api"
+import { execSync } from "child_process"
 
+export default function Command() {
+    let output = execSync("/opt/homebrew/bin/mpc next", { encoding: "utf8" });
+    return (
+        <Detail markdown={output} />
+    )
+}
